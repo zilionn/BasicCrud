59ac398817a846784805f6044c948549bffe5744
zilionn <correiafernando165@gmail.com>
2024-04-23 08:59:12 -0300

    Add project files.

---
+++ BasicCrud/Models/Singer.cs
@@ -0,0 +1,12 @@
+﻿namespace BasicCrud.Models {
+    public class Singer {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public int Age { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
